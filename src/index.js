5d9e4c0e3d3d39043a21292b0dc2b66b1fb689df
balamurugan9655 <shanmugambala80@gmail.com>
2025-02-07 20:37:36 +0530

    Fixed routing issue

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import Contant from './Pages/Contant';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Quiz-App">
       <Routes>
         <Route path="/" element={<App/>}/>
         <Route path="/layout" element={<Layout/>}/>
